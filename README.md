d180f4a1598e098d15139e27b5e00027b66e883e
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-14 10:46:32 +1100

    Draw a horizontal line 2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Introductory-Java-Exercise
+
+
+## Triangle Exercises
+### Easiest exercise ever
+Print one asterisk to the console.
+Example:
+*
+
+### Draw a horizontal line
+Given a number n, print n asterisks on one line.
+Example when n=8:
+********
+
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
-
 /**
  * Created by nataliadjohari on 14/02/2017.
  */
 public class Triangle {
+    /**
+     * Print one asterisk to the console.
+     */
+    public static void asteriskPrint() {
+        System.out.print("*");
+    }
+
+    /**
+     * Given a number n, print n asterisks on one line.
+     * @param n
+     */
+    public static void horizontalLine(int n) {
+        for (int i = 0; i < n; i++)
+            asteriskPrint();
+        System.out.println();
+    }
+
 }
